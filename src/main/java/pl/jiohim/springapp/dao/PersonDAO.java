3a1e8e426fe9b359497064bfc36e1d657ccc3f40
Jiohim <bohdankrasin@gmail.com>
2022-09-12 11:31:04 +0200

    adding PATCH and DELETE

--- src/main/java/pl/jiohim/springapp/dao/PersonDAO.java
+++ src/main/java/pl/jiohim/springapp/dao/PersonDAO.java
@@ -7,7 +7,6 @@ import pl.jiohim.springapp.models.Person;
 import java.util.ArrayList;
 import java.util.List;
 
-
 @Component
 public class PersonDAO {
     private static int PEOPLE_COUNT;
@@ -29,4 +28,18 @@ public class PersonDAO {
     public Person show(int id) {
         return people.stream().filter(person -> person.getId() == id).findAny().orElse(null);
     }
+
+    public void save(Person person) {
+        person.setId(++PEOPLE_COUNT);
+        people.add(person);
+    }
+    public void update(int id , Person updatedPerson) {
+        Person personToBeUpdated = show(id);
+        personToBeUpdated.setName(updatedPerson.getName());
+    }
+
+    public void delete(int id){
+        people.removeIf(p -> p.getId() == id);
+    }
+
 }
