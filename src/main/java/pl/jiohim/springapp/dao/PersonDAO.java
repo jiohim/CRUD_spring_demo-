4107b93630361046c5c940312d7e9eabe9e0024c
Jiohim <bohdankrasin@gmail.com>
2022-09-14 11:04:43 +0200

    adding validation

--- src/main/java/pl/jiohim/springapp/dao/PersonDAO.java
+++ src/main/java/pl/jiohim/springapp/dao/PersonDAO.java
@@ -15,10 +15,10 @@ public class PersonDAO {
     {
         people = new ArrayList<>();
 
-        people.add(new Person(++PEOPLE_COUNT, "Tom"));
-        people.add(new Person(++PEOPLE_COUNT, "Bob"));
-        people.add(new Person(++PEOPLE_COUNT, "Mike"));
-        people.add(new Person(++PEOPLE_COUNT, "Katy"));
+        people.add(new Person(++PEOPLE_COUNT, "Tom" , 24, "Tom@mail.com"));
+        people.add(new Person(++PEOPLE_COUNT, "Bob", 34, "BobSnail@mail.com"));
+        people.add(new Person(++PEOPLE_COUNT, "Mike", 25, "Mike@mail.com"));
+        people.add(new Person(++PEOPLE_COUNT, "Katy", 18, "Kate@mail.com"));
     }
 
     public List<Person> index() {
@@ -36,6 +36,8 @@ public class PersonDAO {
     public void update(int id , Person updatedPerson) {
         Person personToBeUpdated = show(id);
         personToBeUpdated.setName(updatedPerson.getName());
+        personToBeUpdated.setAge(updatedPerson.getAge());
+        personToBeUpdated.setEmail(updatedPerson.getEmail());
     }
 
     public void delete(int id){
